77da119a9de5edfaea37f0eba630eab7e2af2f08
Sumajirou <4375490+sumajirou@users.noreply.github.com>
2020-07-30 05:54:58 +0900

    [style] themeをサイゼ色に変更

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,19 +1,12 @@
 import { createMuiTheme } from "@material-ui/core/styles";
+import { green, red } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
   palette: {
-    primary: {
-      light: "#757ce8",
-      main: "#3f50b5",
-      dark: "#002884",
-      contrastText: "#fff",
-    },
-    secondary: {
-      light: "#ff7961",
-      main: "#f44336",
-      dark: "#ba000d",
-      contrastText: "#000",
-    },
+    // primary: green,
+    // secondary: red,
+    primary: red,
+    secondary: green,
   },
 });
 
