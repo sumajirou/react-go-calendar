2a2c63b073f12581948c8c6ae0ae6755d90f8438
Sumajirou <4375490+sumajirou@users.noreply.github.com>
2020-07-30 03:35:34 +0900

    [add] material-ui themeの導入

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,14 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "fontsource-roboto";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
